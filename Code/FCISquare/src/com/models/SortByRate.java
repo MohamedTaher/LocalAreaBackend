8e1689684b77f334394aeca997d25533ce1e954d
Mohamed Taher <mtaherhassan95@gmail.com>
2016-04-20 20:48:04 +0300

    Revert "add placemodel and checkin2"

--- Code/FCISquare/src/com/models/SortByRate.java
+++ Code/FCISquare/src/com/models/SortByRate.java
@@ -4,22 +4,21 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 
-public class SortByCheckin implements Sort{
+public class SortByRate implements Sort{
 
-	public SortByCheckin() {
+	public SortByRate() {
 		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	public ArrayList<Checkin> sort(ArrayList<Checkin> list) {
-		// TODO Auto-generated method stub
 		Collections.sort(list, new Comparator<Checkin>() {
 
 			@Override
 			public int compare(Checkin o1, Checkin o2) {
-				if(o1.getCheckinPlace().getNumberOfCheckins() > o2.getCheckinPlace().getNumberOfCheckins())
+				if(o1.getCheckinPlace().getRateSum() * o2.getCheckinPlace().getUserNum() > o2.getCheckinPlace().getRateSum() * o1.getCheckinPlace().getUserNum())
 					return 1;
-				else if(o1.getCheckinPlace().getNumberOfCheckins() < o2.getCheckinPlace().getNumberOfCheckins())
+				else if(o1.getCheckinPlace().getRateSum() * o2.getCheckinPlace().getUserNum() < o2.getCheckinPlace().getRateSum() * o1.getCheckinPlace().getUserNum())
 					return -1;
 				return 0;
 			}
